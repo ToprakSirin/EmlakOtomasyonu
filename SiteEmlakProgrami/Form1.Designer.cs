c4ed3c332bfc76db5a36b816d7bba566c74848a7
ToprakSirin <sirintprk21@gmail.com>
2021-09-30 20:50:17 +0300

    Add project files.

---
+++ SiteEmlakProgrami/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace SiteEmlakProgrami
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(461, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(103, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(282, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(94, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(282, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(305, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(692, 425);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Site Giriş Paneli";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SiteEmlakProgrami/Form2.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SiteEmlakProgrami
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-MKCV300;Initial Catalog=siteler;Integrated Security=True");
+
+        private void verilerigoster()
+        {
+            listView1.Items.Clear();
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Select *from sitebilgiler", baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["id"].ToString();
+                ekle.SubItems.Add(oku["site"].ToString());
+                ekle.SubItems.Add(oku["satkira"].ToString());
+                ekle.SubItems.Add(oku["oda"].ToString());
+                ekle.SubItems.Add(oku["metre"].ToString());
+                ekle.SubItems.Add(oku["fiyat"].ToString());
+                ekle.SubItems.Add(oku["blok"].ToString());
+                ekle.SubItems.Add(oku["no"].ToString());
+                ekle.SubItems.Add(oku["adsoyad"].ToString());
+                ekle.SubItems.Add(oku["telefon"].ToString());
+                ekle.SubItems.Add(oku["notlar"].ToString());
+
+                listView1.Items.Add(ekle);
+
+
+            }
+            baglan.Close();
+
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (comboBox1.Text == "Zambak Sitesi")
+            {
+                button1.BackColor = Color.Pink;
+                button2.BackColor = Color.Goldenrod;
+                button3.BackColor = Color.Goldenrod;
+                button4.BackColor = Color.Goldenrod;
+            }
+
+            if (comboBox1.Text == "Papatya Sitesi")
+            {
+                button1.BackColor = Color.Goldenrod;
+                button2.BackColor = Color.Pink;
+                button3.BackColor = Color.Goldenrod;
+                button4.BackColor = Color.Goldenrod;
+            }
+
+            if (comboBox1.Text == "Gül Sitesi")
+            {
+                button1.BackColor = Color.Goldenrod;
+                button2.BackColor = Color.Goldenrod;
+                button3.BackColor = Color.Pink;
+                button4.BackColor = Color.Goldenrod;
+            }
+
+            if (comboBox1.Text == "Menekşe Sitesi")
+            {
+                button1.BackColor = Color.Goldenrod;
+                button2.BackColor = Color.Goldenrod;
+                button3.BackColor = Color.Goldenrod;
+                button4.BackColor = Color.Pink;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGoruntule_Click(object sender, EventArgs e)
+        {
+            verilerigoster();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+
+
+            SqlCommand komut = new SqlCommand("insert into sitebilgiler (id,site,satkira,oda,metre,fiyat,blok,no,adsoyad,telefon,notlar) values('" + textBox8.Text.ToString() + "','" + comboBox1.Text.ToString() + "','" + comboBox2.Text.ToString() + "','" + comboBox3.Text.ToString() + "','" + textBox1.Text.ToString() + "','" + textBox2.Text.ToString() + "','" + comboBox4.Text.ToString() + "','" + textBox7.Text.ToString() + "','" + textBox4.Text.ToString() + "','" + textBox5.Text.ToString() + "','" + textBox6.Text.ToString() + "')", baglan);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoster();
+        
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        int id = 0;
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Delete from sitebilgiler where id=(" + id + ")", baglan);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoster();
+
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            textBox8.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            comboBox1.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            comboBox2.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            comboBox3.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            textBox1.Text = listView1.SelectedItems[0].SubItems[4].Text;
+            textBox2.Text = listView1.SelectedItems[0].SubItems[5].Text;
+            comboBox4.Text = listView1.SelectedItems[0].SubItems[6].Text;
+            textBox7.Text = listView1.SelectedItems[0].SubItems[7].Text;
+            textBox4.Text = listView1.SelectedItems[0].SubItems[8].Text;
+            textBox5.Text = listView1.SelectedItems[0].SubItems[9].Text;
+            textBox6.Text = listView1.SelectedItems[0].SubItems[10].Text;
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("update sitebilgiler set id='" + textBox8.Text.ToString() + "',site='" + comboBox1.Text.ToString() + "',satkira='" + comboBox2.Text.ToString() + "',oda='" + comboBox3.Text.ToString() + "',metre='" + textBox1.Text.ToString() + "',fiyat='" + textBox2.Text.ToString() + "',blok='" + comboBox4.Text.ToString() +  "',no='" + textBox7.Text.ToString() + "',adsoyad='" + textBox4.Text.ToString() + "',telefon='" + textBox5.Text.ToString() + "',notlar='" + textBox6.Text.ToString() + "'where id=" + id + "", baglan);
+     
+           komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoster();
+        }
+    }
+}
